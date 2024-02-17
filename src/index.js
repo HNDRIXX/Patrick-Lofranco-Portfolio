228ce20b71de1c3f9d6d70f74397a076de2a4d12
Patrick Lofranco <patricklofranco49@gmail.com>
2024-02-17 12:18:22 +0800

    Update Portfolio 02/17

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from "./App"
-
+  /* https://HNDRIXX.github.io/Patrick-Lofranco-Portfolio */
+  
 // ReactDOM.render(<App />, document.getElementById('root'))
 ReactDOM.createRoot(document.querySelector("#root")).render(<React.StrictMode><App /></React.StrictMode>);
 
